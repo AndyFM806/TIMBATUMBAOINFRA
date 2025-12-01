3a0f461981a801526995e4ec53cf37cdd2a8eb23
leefernandez1245 <leefernandez1245@gmail.com>
2025-12-01 16:55:39 +0000

    asasas

--- infra/modules/inscripcionesLambda/outputs.tf
+++ infra/modules/inscripcionesLambda/outputs.tf
@@ -1,9 +1,9 @@
 output "lambda_function_arn" {
   description = "The ARN of the Lambda function."
-  value       = aws_lambda_function.lambda.arn
+  value       = aws_lambda_function.inscripciones.arn
 }
 
 output "lambda_function_name" {
   description = "The name of the Lambda function."
-  value       = aws_lambda_function.lambda.function_name
+  value       = aws_lambda_function.inscripciones.function_name
 }
