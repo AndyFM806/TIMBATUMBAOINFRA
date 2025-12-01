c95e8d80f8b8ec69d76c09a84cb4661e7eb48aeb
leefernandez1245 <leefernandez1245@gmail.com>
2025-12-01 16:07:30 +0000

    apply

--- infra/main.tf
+++ infra/main.tf
@@ -14,14 +14,12 @@ provider "aws" {
 }
 
 # ------------------------------------------------------------------------------
-# RECURSOS PRINCIPALES (SQS, DynamoDB, SNS)
+# RECURSOS PRINCIPALES (SNS)
 # ------------------------------------------------------------------------------
 
 module "core_resources" {
   source = "./modules/timbatumbao_resources"
 
-  sqs_queue_name                 = var.sqs_queue_name
-  dynamodb_table_name            = var.dynamodb_table_name
   sns_notifications_topic_name = var.sns_notifications_topic_name
 }
 
@@ -101,7 +99,7 @@ module "enrollment_handler" {
 
   function_name     = "EnrollmentRequestHandler"
   source_file       = "../App/lambdas/initial/index.js"
-  payment_queue_url = module.core_resources.timbatumbao_queue_url
+  payment_queue_url = aws_sqs_queue.lambda_pagos_dlq.id # Corregido para apuntar a la cola correcta
   kms_key_arn       = aws_kms_key.encryption_key.arn
 }
 
@@ -110,8 +108,8 @@ module "payment_processor" {
 
   processor_function_name = "PaymentProcessor"
   processor_source_file   = "../App/lambdas/pagos/main.py"
-  payment_queue_arn       = module.core_resources.timbatumbao_queue_arn
-  dynamodb_table_name     = module.core_resources.timbatumbao_table_name
+  payment_queue_arn       = aws_sqs_queue.lambda_pagos_dlq.arn # Corregido para apuntar a la cola correcta
+  dynamodb_table_name     = aws_dynamodb_table.inscripciones_table.name
   sns_topic_arn           = module.core_resources.timbatumbao_notifications_topic_arn
   processor_kms_key_arn   = aws_kms_key.encryption_key.arn
 }
--- infra/modules/timbatumbao_resources/outputs.tf
+++ infra/modules/timbatumbao_resources/outputs.tf
@@ -5,27 +5,7 @@ output "timbatumbao_kms_key_arn" {
   value       = aws_kms_key.timbatumbao_key.arn
 }
 
-output "timbatumbao_table_name" {
-  description = "Nombre de la tabla DynamoDB para las inscripciones"
-  value       = aws_dynamodb_table.timbatumbao_table.name
-}
-
-output "timbatumbao_table_arn" {
-  description = "ARN de la tabla DynamoDB para las inscripciones"
-  value       = aws_dynamodb_table.timbatumbao_table.arn
-}
-
-output "timbatumbao_queue_url" {
-  description = "URL de la cola SQS para procesar pagos"
-  value       = aws_sqs_queue.payment_queue.id
-}
-
-output "timbatumbao_queue_arn" {
-  description = "ARN de la cola SQS para procesar pagos"
-  value       = aws_sqs_queue.payment_queue.arn
-}
-
 output "timbatumbao_notifications_topic_arn" {
-  description = "ARN del tema SNS para notificar los resultados de la inscripción"
+  description = "ARN del tema SNS para notificaciones"
   value       = aws_sns_topic.timbatumbao_notifications.arn
 }
