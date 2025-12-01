6463778742e8d1d861536aafaab4e4b69a42c3ee
leefernandez1245 <leefernandez1245@gmail.com>
2025-12-01 16:52:55 +0000

    sss

--- infra/modules/inscripcionesLambda/outputs.tf
+++ infra/modules/inscripcionesLambda/outputs.tf
@@ -1,15 +1,9 @@
-output "lambda_arn" {
-  description = "ARN de la función Lambda de Inscripciones."
-  value       = aws_lambda_function.inscripciones.arn
+output "lambda_function_arn" {
+  description = "The ARN of the Lambda function."
+  value       = aws_lambda_function.lambda.arn
 }
 
-output "lambda_name" {
-  description = "Nombre de la función Lambda de Inscripciones."
-  value       = aws_lambda_function.inscripciones.function_name
-}
-
-# El rol puede ser útil si otro recurso necesita permiso para asumir el rol de esta Lambda.
-output "lambda_role_arn" {
-  description = "ARN del rol de ejecución de la función Lambda."
-  value       = aws_iam_role.lambda_exec.arn
+output "lambda_function_name" {
+  description = "The name of the Lambda function."
+  value       = aws_lambda_function.lambda.function_name
 }
--- infra/modules/lambda_processor/outputs.tf
+++ infra/modules/lambda_processor/outputs.tf
@@ -1,14 +1,9 @@
 output "lambda_function_arn" {
-  description = "El ARN de la función Lambda."
-  value       = aws_lambda_function.this.arn
+  description = "The ARN of the Lambda function."
+  value       = aws_lambda_function.lambda.arn
 }
 
 output "lambda_function_name" {
-  description = "El nombre de la función Lambda."
-  value       = aws_lambda_function.this.function_name
-}
-
-output "lambda_iam_role_arn" {
-  description = "El ARN del rol de IAM de la Lambda."
-  value       = aws_iam_role.lambda_exec.arn
+  description = "The name of the Lambda function."
+  value       = aws_lambda_function.lambda.function_name
 }
